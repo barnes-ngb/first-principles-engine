b355378d52ece0082f9f2c3076ffb0ee33d35751
Nathan Barnes <barnes.ngb@gmail.com>
2026-02-06 23:57:42 -0600

    Add MUI theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { theme } from './app/theme'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
